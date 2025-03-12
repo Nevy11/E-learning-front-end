2b35952658bf549a950f056cfb1f7cab03f8d76e
nevy11 <smongare2003@gmail.com>
2025-03-12 19:03:46 +0300

    update sign up title in small, medium, and large screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'sign up',
+    component: SignUpComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'sign up',
+    pathMatch: 'full',
+  },
+];

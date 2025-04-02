9f2f0e3bb5af8203837c75c4dcf1faffd93cac26
nevy11 <smongare2003@gmail.com>
2025-04-02 16:35:18 +0300

    initialize login-form

---
+++ src/app/login/login-form/login-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nevy11-login-form',
+  imports: [],
+  templateUrl: './login-form.component.html',
+  styleUrl: './login-form.component.scss'
+})
+export class LoginFormComponent {
+
+}
